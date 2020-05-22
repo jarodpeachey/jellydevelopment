dec30460cbd98e58791593e01854caeec6a7714a
Jarod Peachey <jwpeachey107@aol.com>
2020-05-22 18:15:15 -0400

    Update site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Jelly Development - Custom JAMstack Websites and Solutions',
     description:
-      'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
+      'JAMstack website development. Custom website solutions and integrations for businesses and developers.',
     author: '@gatsbyjs',
   },
   plugins: [
