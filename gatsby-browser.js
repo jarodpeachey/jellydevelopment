60491644df642e75347c67fd5a12cf44c11153ca
Jarod Peachey <jwpeachey107@aol.com>
2020-05-30 07:33:06 -0400

    Add self-hosted fonts

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StylesProvider } from './src/providers/StylesProvider';
 import { AppProvider } from './src/providers/AppProvider';
+import 'typeface-roboto-mono';
 
 export const wrapRootElement = ({ element }) => {
   console.log(element);
