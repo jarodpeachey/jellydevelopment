e5cdbe47ca73d375bb872f665faccf91f6541153
Jarod Peachey <jwpeachey107@aol.com>
2020-06-02 20:21:36 -0400

    Add cache plugins

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { StylesProvider } from './src/providers/StylesProvider';
 import { AppProvider } from './src/providers/AppProvider';
 import 'typeface-nunito-sans';
+import Layout from './src/components/layout/Layout';
 
 export const wrapRootElement = ({ element }) => {
   console.log(element);
 
   return (
     <AppProvider>
-      <StylesProvider>{element}</StylesProvider>
+      <StylesProvider>
+        <Layout>{element}</Layout>
+      </StylesProvider>
     </AppProvider>
   );
 };
